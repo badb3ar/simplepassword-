bb96fa4e3ecc64ae5a9c14e5b4b6202a6fd3e07e
b@dbear <30443732+badb3ar@users.noreply.github.com>
2017-08-21 00:31:54 +0600

    Add files via upload

---
+++ password.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	
+  long pass;
+do
+{	
+	cout<<"Enter your Password:";
+	cin>>pass;
+	
+	if (pass== 1010)
+	{
+		cout<<"Welcome !"<<endl;
+	}
+	else 
+	{
+		cout<<"Access Denided!"<<endl<<endl;
+	}
+}
+	while (pass != 1010);
+	getchar();
+	return 0;
+}
